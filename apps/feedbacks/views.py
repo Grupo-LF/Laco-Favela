af2889e9bb1163c009946a047d0d57a634998a7f
Victor Carraly <vcarraly@gmail.com>
2026-05-24 20:26:35 -0300

    Adiciona lógica de envio e recebimento de feedbacks
    Adiciona a lógica de envio e recebimento de feedbacks, de filtragem dos dados e impede qualquer um além do admin de ler, excluir e editar os feedbacks

--- apps/feedbacks/views.py
+++ apps/feedbacks/views.py
@@ -1,8 +1,34 @@
 from rest_framework import viewsets
+from rest_framework.permissions import AllowAny, IsAdminUser
+from rest_framework.filters import OrderingFilter
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from .models import Feedback
 from .serializers import FeedbackSerializer
+from django_filters.rest_framework import DjangoFilterBackend
+
 
 # Create your views here.
 class FeedbackViewSet(viewsets.ModelViewSet):
     queryset = Feedback.objects.all()
     serializer_class = FeedbackSerializer
+    
+    filter_backends = [OrderingFilter, DjangoFilterBackend]
+    filterset_fields = ['lido', 'tipo']
+    ordering_fields = ['criado_em']
+    ordering = ['-criado_em']
+
+    def get_permissions(self):
+        if self.action == 'create':
+            permission_classes = [AllowAny]
+        else:
+            permission_classes = [IsAdminUser]
+        
+        return [permission() for permission in permission_classes]
+    
+    @action(detail=True, methods=['patch'])
+    def marcar_lido(self, request, pk=None):
+        feedback = self.get_object()
+        feedback.lido = True
+        feedback.save()
+        return Response({'status': 'Feedback marcado como lido com sucesso!'})
