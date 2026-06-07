3b818a44dcd3c5b576ffba09773da042456c2831
leticia camello barros <lsncb@cesar.school>
2026-06-07 17:58:50 -0300

    RegistrosdeVisitas

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,20 +4,23 @@ import AppAdmin from './AppAdmin';
 import AppPresidente from './AppPresidente';
 import AppMorador from './AppMorador';
 import Login from './pages/login/Login';
-function App(onLogin) {
+
+function App() {
   const [tipoUsuario, setTipoUsuario] = useState(localStorage.getItem('tipo'));
 
-  const handleLogin = (tipo, token,nome) => {
+  const handleLogin = (tipo, token, nome) => {
     localStorage.setItem('token', token);
     localStorage.setItem('tipo', tipo);
-    localStorage.setItem('nome', nome); // Supondo que o nome seja retornado como token, ajuste conforme necessário
+    localStorage.setItem('nome', nome);
     setTipoUsuario(tipo);
   };  
+  
   const handleLoginTest = (tipo) => {
     localStorage.setItem('tipo', tipo);
     console.log('Tipo de usuário salvo no localStorage (modo teste):', tipo);
     setTipoUsuario(tipo);
   }
+  
   // Se não tem tipo, mostra login
   if (!tipoUsuario) return <Login onLogin={handleLogin} handleLoginTest={handleLoginTest} />;
 
